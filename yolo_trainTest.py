02ec6f844f487c2653b968c4d96a92bfd3f084f1
hrmoradi <hrmoradi@gmail.com>
2023-02-28 14:14:03 -0600

    Hamid: Yolov7 working

--- yolo_trainTest.py
+++ yolo_trainTest.py
@@ -5,5 +5,7 @@ import os
 import argparse
 
 
+# using #  OAI_pain\yolov7\data\coco.yaml
+
 
 os.system("cd yolov7/ && py train.py --workers 1  --batch-size 16 --epochs 100 --img 640 640 --hyp data/hyp.scratch.custom.yaml  --name yolov7-custom --weights yolov7.pt")
