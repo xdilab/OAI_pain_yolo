f62216b0728aec2ec81457764b23c03c2228bdfa
Hunter Sylvester <huntersylvester@Hunters-MacBook-Pro-10.local>
2023-03-06 16:38:13 -0600

    Hunter : update

--- 3d_model_img_loader.py
+++ 3d_model_img_loader.py
@@ -1,16 +1,4 @@
-# Moradi's original code/idea
-# from PIL import Image
-# import numpy as np
-
-# image_list = []
-
-# for i in range(num_layers):  #
-#     filename = "layer" + str(i) + ".jpg"
-#     im = Image.open(filename)
-#     image_list.append(im)
-
-# image_array = np.array([np.array(im) for im in image_list])
-
+hrmoradi = True  # TODO:  if Hamidreza Moradi = True  <<<<<<<<<<<  Hunter = False
 
 import fnmatch
 import glob
@@ -21,47 +9,38 @@ import numpy as np
 
 # Empty list to store numpy arrays
 image_list = []
-hrmoradi = True  # TODO:  if Hamidreza Moradi = True  <<<<<<<<<<<  Hunter = False
+y = []
 
 # lets us know number of images in that file
 dirpath = "/Users/huntersylvester/Desktop/UMMC/Research/Moradi/sample_knee_mri_localization/9009067_10287112_R_Progressed"
 if hrmoradi: dirpath = "C:\\Users\\hrmor\OneDrive - University of Mississippi Medical Center\\05_Pycharm\\OAI_pain\\sample_knee_mri_localization\\9009067_10287112_R_Progressed\\"
-
 num_layers = len(fnmatch.filter(os.listdir(dirpath), '*.jpg'))
-print("num_layers for a patient: ", num_layers)
+print(num_layers)
 
 # This will walk through files in directory and rename jpgs in correct order
-# os.chdir(dirpath)
 directory = "/Users/huntersylvester/Desktop/UMMC/Research/Moradi/sample_knee_mri_localization"
 if hrmoradi: directory = "C:\\Users\\hrmor\OneDrive - University of Mississippi Medical Center\\05_Pycharm\\OAI_pain\\sample_knee_mri_localization\\"
-
-print("len directories: ", len(list(glob.iglob(f"{directory}/*"))))
 for filename in glob.iglob(f"{directory}/*"):
-    print(filename)
     i = 0
-    x = os.path.join(directory,
-                     filename)  # TODO: why join? for me the addresses are the same for x and filename (if different on your system disregard)
-    print(x)
-    os.chdir(x)
-    for file in natsorted(glob.glob("*.jpg")):  # TODO: good library!
+    os.chdir(filename)
+    x = filename.find('_NotProgressed')  # Will give -1 if phrase not in string
+    if x > 0:
+        y.append(0)  # non progressed 0
+    else:
+        y.append(1)  # progressed 1
+    image_patient = []
+    for file in natsorted(glob.glob("*.jpg")):
         i += 1
-        filename = "layer" + str(i) + ".jpg"
-        print(f"{i} + {filename}")
-        os.rename(file,
-                  filename)  # TODO: there is no need to rename them! keep original names so later we can refer, just sort and keep in numpy
-        im = Image.open(filename)
-        image_list.append(im)
+        im = Image.open(file)
+        image_patient.append(np.array(im))
         if i != num_layers:
             pass
         else:
             print(f"number of layers: {i}")
+    image_list.append(np.array(image_patient))
 
-image_array = np.array([np.array(im) for im in image_list])
+image_list = np.array(image_list)
 
-print(image_array)
-
-print(len(image_array))
-
-print(image_array.shape)
-# TODO: (3, 11, 384, 384) is better, first dimention patients, second layers, 3/4 th images, then another numpy of size 3 will have labels! [progressed, not progressed, progressed]
-# TODO: which as of now might be easier to read from folder names
+print(len(image_list), len(image_list[0]), len(image_list[0][0]))
+print(type(image_list), type(image_list[0]), type(image_list[0][0]))
+print(y)
